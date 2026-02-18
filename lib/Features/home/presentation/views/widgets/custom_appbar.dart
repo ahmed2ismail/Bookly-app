2e9445754c1e0cd89441371959e3a403f9456852
ahmed2ismail <ah.hany5050@gmail.com>
2026-02-18 22:22:12 +0200

    add futured books list view to home

--- lib/Features/home/presentation/views/widgets/custom_appbar.dart
+++ lib/Features/home/presentation/views/widgets/custom_appbar.dart
@@ -1,4 +1,3 @@
-
 import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -9,7 +8,12 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40),
+      padding: const EdgeInsets.only(
+        left: 24.0,
+        right: 24.0,
+        top: 40,
+        bottom: 20,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
