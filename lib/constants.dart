cd75bb8378730a853c0ddec9604d5fca33fddcd9
ahmed2ismail <ah.hany5050@gmail.com>
2026-02-19 16:42:37 +0200

    create app text styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
 
 const kPrimaryColor = Color(0xff100B20);
-const kFontFamily = 'GT Sectra Fine Regular';
+const kGTSectraFine = 'GT Sectra Fine Regular';
 const kTransitonDuration = Duration(microseconds: 250);
