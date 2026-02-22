2fdb5793b78aec38ba86d7b328143b33a6453b1e
ahmed2ismail <ah.hany5050@gmail.com>
2026-02-22 23:12:46 +0200

    refactor book details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,66 +1,27 @@
-import 'package:bookly_app/Core/utils/styles.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_appbar.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
-import 'books_actions.dart';
-import 'similar_books_list_view.dart';
+import 'books_details_section.dart';
+import 'similar_books_section.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
+    return const CustomScrollView(
+      physics: BouncingScrollPhysics(),
       slivers: [
         SliverFillRemaining(
-          hasScrollBody: false, // عشان لو المحتوي بتاع ال Column اكبر من المساحة المتاحة في ال screen يفضل فيه scroll، ولو اقل من المساحة المتاحة في ال screen يبقي مفيش scroll خالص
+          hasScrollBody:
+              false, // عشان لو المحتوي بتاع ال Column اكبر من المساحة المتاحة في ال screen يفضل فيه scroll، ولو اقل من المساحة المتاحة في ال screen يبقي مفيش scroll خالص
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 40),
+            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 40),
             child: Column(
               children: [
-                const CustomBookDetailsAppBar(),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .2),
-                  child: const CustomBookImage(),
-                ),
-                const SizedBox(height: 43),
-                Text(
-                  'The Jungle Book',
-                  style: Styles.textStyle30.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 6),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: Styles.textStyle18.copyWith(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 18),
-                const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-                const SizedBox(height: 37),
-                const BooksActionButtons(),
-                const Expanded(child: SizedBox(height: 50)),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const SimilarBooksListView(),
+                CustomBookDetailsAppBar(),
+                BookDetailsSection(),
+                Expanded(child: SizedBox(height: 50)),
+                SimilarBooksSection(),
               ],
             ),
           ),
