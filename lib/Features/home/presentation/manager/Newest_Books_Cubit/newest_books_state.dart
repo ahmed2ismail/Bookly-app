860b7d9eb1bcfcaa9754966bdfa6baefd0eea6be
ahmed2ismail <ah.hany5050@gmail.com>
2026-02-26 17:12:58 +0200

    create newest books cubit

---
+++ lib/Features/home/presentation/manager/Newest_Books_Cubit/newest_books_state.dart
@@ -0,0 +1,18 @@
+part of 'newest_books_cubit.dart';
+
+@immutable
+sealed class NewestBooksState {}
+
+final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BooksModel> books;
+  NewestBooksSuccess(this.books);
+}
+
+final class NewestBooksFailure extends NewestBooksState {
+  final String errorMessage;
+  NewestBooksFailure(this.errorMessage);
+}
